536afa1f9159073272aee24ad7f46e7fa525bab6
jogindra kumar <joginder9868@outlook.com>
2022-02-07 19:20:06 +0530

    modified:   resources/views/auth/admin_login.blade.php 	modified:   resources/views/backend/about/add_about.blade.php

--- resources/views/auth/admin_login.blade.php
+++ resources/views/auth/admin_login.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="{{asset('backend/images/favicon.ico')}}">
+    <link rel="icon" href="">
 
-    <title>Ecom - Log in </title>
+    <title>Log In </title>
   
 	<!-- Vendors Style-->
 	<link rel="stylesheet" href="{{asset('backend/css/vendors_css.css')}}">
--- resources/views/backend/about/add_about.blade.php
+++ resources/views/backend/about/add_about.blade.php
@@ -17,33 +17,96 @@
       
     
         <div class="form-group">
-        <h5>Brand Name English<span class="text-danger">*</span></h5>
+        <h5>Name<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="text" name="brand_name_en" class="form-control"  value=""  > 
-		@error('brand_name_en')
+        <input type="text" name="name" class="form-control"  value=""  > 
+		@error('name')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+		</div>
+    </div>
+	 <div class="form-group">
+        <h5>position First<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="text" name="position_first" class="form-control"  value=""  > 
+		@error('position_first')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+		</div>
+    </div>
+	<div class="form-group">
+        <h5>Position second<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="text" name="position_second" class="form-control"  value=""  > 
+		@error('position_first')
 		<span class="text-danger">{{$message}}</span>
 		@enderror
 		</div>
     </div>
     <div class="form-group">
-        <h5>Brand Name Hindi<span class="text-danger">*</span></h5>
+        <h5>Email<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="text" name="brand_name_hin" class="form-control"  value=""  >
-		@error('brand_name_hin')
+        <input type="email" name="email" class="form-control"  value=""  >
+		@error('email')
 		<span class="text-danger">{{$message}}</span>
 		@enderror
 	 </div>
     </div>
     <div class="form-group">
-        <h5>Brand Image<span class="text-danger">*</span></h5>
+        <h5>Description<span class="text-danger">*</span></h5>
         <div class="controls">
-        <input type="file" name="brand_image"  class="form-control"  value=""  > 
-	@error('brand_image')
+        <input type="text" name="desp"  class="form-control"  value=""  > 
+	@error('desp')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
+    </div>
+	<div class="form-group">
+        <h5>job<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="text" name="job"  class="form-control"  value=""  > 
+	@error('job')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
+    </div>
+	<div class="form-group">
+        <h5>skill_name<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="text" name="skill_name"  class="form-control"  value=""  > 
+	@error('skill_name')
 		<span class="text-danger">{{$message}}</span>
 		@enderror
 	</div>
     </div>
    
+	<div class="form-group">
+        <h5>skill_per<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="text" name="skill_per"  class="form-control"  value=""  > 
+	@error('skill_per')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
+    </div>
+	<div class="form-group">
+        <h5>Profile pic<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="file" name="img"  class="form-control"  value=""  > 
+	@error('img')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
+    </div>
+	<div class="form-group">
+        <h5>CV<span class="text-danger">*</span></h5>
+        <div class="controls">
+        <input type="file" name="cv"  class="form-control"  value=""  > 
+	@error('img')
+		<span class="text-danger">{{$message}}</span>
+		@enderror
+	</div>
+    </div>
 
              
    
